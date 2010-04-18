b2e93c559801994331bb52019e0715fcd0bff294
georgy.turevich <georgy.turevich@f9940e21-5314-ac73-b139-a91728cbb21d>
2010-04-18 20:41:05 +0000

    Removing old values of Id keyword
    git-svn-id: http://zf-dojo.googlecode.com/svn/branches/georgy@42 f9940e21-5314-ac73-b139-a91728cbb21d

--- tests/library/Zend/Dojo/View/Helper/AllTests.php
+++ tests/library/Zend/Dojo/View/Helper/AllTests.php
@@ -17,7 +17,7 @@
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id: AllTests.php 20096 2010-01-06 02:05:09Z bkarwin $
+ * @version    $Id$
  */
 
 require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
--- tests/library/Zend/Dojo/View/Helper/TooltipTest.php
+++ tests/library/Zend/Dojo/View/Helper/TooltipTest.php
@@ -17,7 +17,7 @@
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id: TooltipTest.php 20096 2010-01-06 02:05:09Z bkarwin $
+ * @version    $Id$
  */
 
 // Call Zend_Dojo_View_Helper_TooltipTest::main() if this source file is executed directly.
